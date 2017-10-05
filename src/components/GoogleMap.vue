c1ad49d4a961e374fc4fb8e1279977d7cc47c060
Thomas Goris <thomasgoris@hotmail.be>
2017-10-05 14:22:36 +0200

    added pages folder

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="google-map"></div>
+</template>
+
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBFGo6SefyZj7q3hVujTNYqlOIKId4xs-k"></script>
+<script>
+  export default {
+    name: 'google-map',
+    mounted: function(){
+      const element = document.getElementById('map');
+      const options = {
+        zoom: 14,
+        center: new google.maps.LatLng(51.501527,-0.1921837)
+      }
+      const map = new google.maps.Map(element, options);
+    }
+  };
+</script>
---
+++ src/components/pages/Map.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navigation></navigation>
+    <google-map id="map"></google-map>
+    <foot></foot>
+  </div>
+</template>
+
+<script>
+  import Navigation from '@/components/Navigation'
+  import GoogleMap from '@/components/GoogleMap'
+  import Foot from '@/components/Footer'
+
+  export default {
+    name: 'Map',
+    components: {
+      Navigation,
+      GoogleMap,
+      Foot
+    }
+  }
+</script>
